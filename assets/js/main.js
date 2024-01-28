64ae2c23ef9c041d2cbc857888e622e775cfa677
skywilian <codewilian852@gmail.com>
2024-01-28 17:18:44 -0300

    Preenchendo os dados hard-Skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,10 +28,15 @@ function updateSoftSkills(profileData) {
   softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
 }
 
+function updateHardSkills(profileData) {
+  const hardSkills = document.getElementById('hard-skills.skills')
+  hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"`).join('')
+}
+
 (async () => {
   const profileData = await fetchProfileData()
   console.log(profileData)
   updateProfileInfo(profileData)
   updateSoftSkills(profileData)
-
+  updateHardSkills(profileData)
 })()
